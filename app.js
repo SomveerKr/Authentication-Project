575debbfb171d0714c7575300462162170a11416
Somveer Kumar <7somveerkumar@gmail.com>
2022-09-11 15:55:54 +0530

    Authentication Project V2: Databse Encryption & Using Environment Variables

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 
@@ -11,10 +13,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema={
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
-};
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User=new mongoose.model("User", userSchema);
 
@@ -45,18 +50,21 @@ app.post("/register", (req, res)=>{
     })
 })
 app.post("/login", (req, res)=>{
-    User.findOne(
-    {
-        email:req.body.username,
-        password:req.body.password
-    }, 
-    (err, foundUser)=>{
-        if(foundUser){
-            res.render("secrets")
-        } else {
-            res.render("login")
+    const username=req.body.username;
+    const password=req.body.password;
+    User.findOne({ email:username }, 
+        (err, foundUser)=>{
+            if(!err){
+                if(foundUser){
+                    if(foundUser.password==password){
+                        res.render("secrets");
+                    }
+                }
+            } else {
+                console.log(err)
+            }
         }
-    })
+    )
 })
 
 app.listen(3000, ()=>{
